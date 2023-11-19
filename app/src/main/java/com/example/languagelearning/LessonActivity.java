8270286ee3d4e12a57773f69c6907e27c1d12f0a
SwindlerComputer <nelsonude02@gmail.com>
2023-11-19 17:33:14 +0000

    milestone 1 progress done

--- app/src/main/java/com/example/languagelearning/LessonActivity.java
+++ app/src/main/java/com/example/languagelearning/LessonActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,19 +15,44 @@ public class LessonActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lesson);
 
-        // This display a language lesson
-        TextView lessonTextView = findViewById(R.id.lessonTextView);
-        lessonTextView.setText("Lesson: Basic Greetings\nHello, how are you?");
+        // Get the selected language from the intent
+        String selectedLanguage = getIntent().getStringExtra("language");
 
-        // placeholders for interactive exercises
+        // Display a language lesson based on the selected language
+        TextView lessonTextView = findViewById(R.id.lessonTextView);
+        if (selectedLanguage != null) {
+            String lessonContent = getLessonContent(selectedLanguage);
+            lessonTextView.setText(lessonContent);
+        } else {
+            // Handle the case where no language is selected (this should not happen in your flow)
+            Toast.makeText(this, "No language selected", Toast.LENGTH_SHORT).show();
+            finish(); // Close the activity if no language is selected
+        }
+
+        // Placeholders for interactive exercises
         Button quizButton = findViewById(R.id.quizButton);
         quizButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // This will start a quiz or interactive exercise
+                Toast.makeText(LessonActivity.this, "Start Quiz", Toast.LENGTH_SHORT).show();
             }
         });
     }
+
+    private String getLessonContent(String language) {
+        // If user chooses a language it will be dynamic and if they choose english it would be english
+        //if they choose french it would be in french
+        // Testing so far ( thinking of ways to do gestures
+        switch (language) {
+            case "English":
+                return "Lesson: Basic Greetings in English\nHello, Good morning";
+            case "Spanish":
+                return "Lección: Saludos básicos en español\nHola, Buenos días";
+            case "French":
+                return "Leçon: Salutations de base en français\nBonjour, Bonne journée";
+            default:
+                return "Lesson content not available";
+        }
+    }
 }
-
-
--- app/src/main/java/com/example/languagelearning/MainActivity.java
+++ app/src/main/java/com/example/languagelearning/MainActivity.java
@@ -4,10 +4,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button englishButton;
+    private Button spanishButton;
+    private Button frenchButton;
+    private Button lessonButton;
 
     private String selectedLanguage;
 
@@ -16,48 +22,76 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button lessonButton = findViewById(R.id.lessonButton);
+        lessonButton = findViewById(R.id.lessonButton);
+        englishButton = findViewById(R.id.englishButton);
+        spanishButton = findViewById(R.id.spanishButton);
+        frenchButton = findViewById(R.id.frenchButton);
+
         lessonButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (selectedLanguage != null) {
                     launchLessonActivity(selectedLanguage);
                 } else {
-
+                    // Display a toast message if no language is selected when the user tries to start lesson
+                    Toast.makeText(MainActivity.this, "Please select a language", Toast.LENGTH_SHORT).show();
                 }
             }
         });
 
-       // languages chosen so far
-        Button englishButton = findViewById(R.id.englishButton);
         englishButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                selectedLanguage = "English";
+                handleLanguageButtonClick("English", englishButton);
             }
         });
 
-        Button spanishButton = findViewById(R.id.spanishButton);
         spanishButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                selectedLanguage = "Spanish";
+                handleLanguageButtonClick("Spanish", spanishButton);
             }
         });
 
-        Button frenchButton = findViewById(R.id.frenchButton);
         frenchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                selectedLanguage = "French";
+                handleLanguageButtonClick("French", frenchButton);
             }
         });
     }
-    //This is for the selected language e.g english
+
+    private void handleLanguageButtonClick(String language, Button selectedButton) {
+        // Display a toast message, If I select a language e.g english it will say " You have selected english"
+        Toast.makeText(MainActivity.this, "You have selected " + language, Toast.LENGTH_SHORT).show();
+
+        // Highlights button selected
+        resetButtonHighlights();
+        selectedButton.setBackgroundColor(getResources().getColor(android.R.color.holo_green_light));
+
+        // Sets the selected language
+        selectedLanguage = language;
+    }
+
+    private void resetButtonHighlights() {
+        // Reset the background color of all buttons
+        englishButton.setBackgroundColor(getResources().getColor(android.R.color.transparent));
+        spanishButton.setBackgroundColor(getResources().getColor(android.R.color.transparent));
+        frenchButton.setBackgroundColor(getResources().getColor(android.R.color.transparent));
+    }
+
+    // This is for the selected language, e.g., English
     private void launchLessonActivity(String language) {
-        //This navigates from main to lesson activity
+        // This navigates from main to lesson activity
         Intent intent = new Intent(MainActivity.this, LessonActivity.class);
         intent.putExtra("language", language);
         startActivity(intent);
+    }
+
+    // If user selects start quiz they are able to go back with the button when pressed
+    @Override
+    public void onBackPressed() {
+        // Handle back button press (e.g., navigate back to MainActivity)
+        super.onBackPressed();
     }
 }
